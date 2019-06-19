9c6ccfa477c45c835810ae8aed5eef760a1ca77f
Chenhao Wu <cwu11@sheffield.ac.uk>
2019-06-19 23:07:58 +0100

    add json and jsonparser python file

---
+++ Google-Assistant/parserJsonMultiquery.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 16:53:54 2019
+
+@author: wuchenhao
+"""
+import json
+
+def readJson(f):
+    file = open(f,'r')
+    
+    jdson = str(file.read())
+    jsonData = json.loads(jdson)
+    queryPatterns = []
+    for index in range(0,len(jsonData['actions'])):
+        
+        queryPattern = jsonData['actions'][index]['intent']['trigger']['queryPatterns']
+        print(queryPattern)
+        queryPatterns += queryPattern
+    
+    return (queryPatterns)
+
+queryPatterns = readJson('team2.json')
+
